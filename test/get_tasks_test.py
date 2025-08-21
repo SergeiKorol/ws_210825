c042cd1d5f4966c40ea333ea24ab0406b39ff448
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2025-08-21 20:05:57 +0300

    123

--- test/get_tasks_test.py
+++ test/get_tasks_test.py
@@ -1,6 +1,9 @@
 import requests
 
 def get_tasks_test():
+    """
+    Это тест про тест
+    """
     response = requests.get("https://todo-app-sky.herokuapp.com/")
     assert response.status_code == 200
     assert type(response.json()) is list
