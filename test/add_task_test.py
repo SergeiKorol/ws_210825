b912bcec88d2c22b54f983b0dfafb28f89b8cdee
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2025-08-21 20:01:32 +0300

    123

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -2,7 +2,7 @@ import requests
 
 def test_add():
     body = {"title":"generated","completed":False}
-    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    response = requests.post(url="https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
     
     assert response.status_code == 202
--- test/get_tasks_test.py
+++ test/get_tasks_test.py
@@ -4,3 +4,9 @@ def get_tasks_test():
     response = requests.get("https://todo-app-sky.herokuapp.com/")
     assert response.status_code == 200
     assert type(response.json()) is list
+
+
+def get_tasks_test1():
+    response = requests.get("https://todo-app-sky.herokuapp.com/")
+    assert response.status_code == 200
+    assert type(response.json()) is list
